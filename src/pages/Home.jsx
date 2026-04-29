901117a3a8b4c5cc72668656e022aaa10585fe7b
ayoub <ayoubbelaoud@gmail.com>
2026-04-29 15:26:22 +0000

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Greetings from '../components/home/Greetings'
 import MiniCard from '../components/home/MiniCard'
 import {BsCashCoin} from "react-icons/bs"
 import {GrInProgress} from "react-icons/gr"
+import RecentOrders from '../components/home/RecentOrders'
 
 const Home = () =>{
     return(
@@ -12,9 +13,10 @@ const Home = () =>{
             <div className='flex-[3] '>
                <Greetings />
                <div className='flex items-center w-full gap-3 px-8 mt-8'>
-                    <MiniCard title="Total Earnings" icon={<BsCashCoin />} number={512}/>
-                    <MiniCard title="In Progress" icon={<GrInProgress />} number={16}/>
+                    <MiniCard title="Total Earnings" icon={<BsCashCoin />} number={512} footerNum={1.6}/>
+                    <MiniCard title="In Progress" icon={<GrInProgress />} number={16} footerNum={3.6}/>
                </div>
+               <RecentOrders />
             </div>
             {/*Right Div*/}
             <div className='flex-[2] bg-blue-600'>

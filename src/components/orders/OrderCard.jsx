bc87edb6afda06051b63d131b4b479979ae59a3c
ayoub <ayoubbelaoud@gmail.com>
2026-05-04 14:42:44 +0000

    update

---
+++ src/components/orders/OrderCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {FaCheckDouble} from 'react-icons/fa'
+import { FaCircle } from 'react-icons/fa'
+
+const OrderCard = () => {
+    return (
+        <div className='w-[550px] bg-[#262626] p-4 rounded-lg mb-4'>
+                    <button className='bg-[#f6b100] p-3 text-xl font-bold rounded-lg'>AM</button>
+                    <div className='flex items-center justify-between w-[100%]'> 
+                        <div className='flex flex-col items-end gap-2'>
+                            <h1 className='text-[#f5f5f5] text-lg font-semibold tracking-wide'>Amrit Raj</h1>
+                            <p className='text-[#ababab] text-sm'>#101/Dine in</p>
+                        </div>
+                        
+                        <div className='flex flex-col items-start gap-2'>
+                            <p className='text-green-600 bg-[#2e4a40] px-2 py-1 rounded-lg'> <FaCheckDouble className='inline mr-2' />Ready</p>
+                            <p className='text-[#ababab] text-sm'><FaCircle className="inline mr-2 text-green-600" />Ready to serve</p>
+                        </div>
+                    </div>
+                    <div className='flex justify-between items-center mt-4 text-[#ababab]'>
+                        <p>Mai 04, 2026 10:44 PM</p>
+                        <p>8 Items</p>
+                    </div>
+                    <hr className='w-full mt-4 border-t-1 border-gray-500' />
+                    <div className='flex items-center justify-between mt-4'>
+                        <h1 className='text-[#f5f5f5] text-lg font-semibold'>Total</h1>
+                        <p className='text-[#f5f5f5] text-lg font-semiBold'>250.00dh</p>
+                    </div>
+        </div>
+    )
+}
+
+export default OrderCard
---
+++ src/constants/index.js
@@ -0,0 +1,50 @@
+export const popularDishes = [
+    {
+        id: 1,
+        image: "src/assets/images.jpg",
+        name: 'Tajine poulet',
+        numberOfOrders: 310,
+    },
+    {
+        id: 1,
+        image: "src/assets/images.jpg",
+        name: 'Tajine poulet',
+        numberOfOrders: 310,
+    },
+    {
+        id: 1,
+        image: "src/assets/images.jpg",
+        name: 'Tajine poulet',
+        numberOfOrders: 310,
+    },
+    {
+        id: 1,
+        image: "src/assets/images.jpg",
+        name: 'Tajine poulet',
+        numberOfOrders: 310,
+    },
+    {
+        id: 1,
+        image: "src/assets/images.jpg",
+        name: 'Tajine poulet',
+        numberOfOrders: 310,
+    },
+    {
+        id: 1,
+        image: "src/assets/images.jpg",
+        name: 'Tajine poulet',
+        numberOfOrders: 310,
+    },
+    {
+        id: 1,
+        image: "src/assets/images.jpg",
+        name: 'Tajine poulet',
+        numberOfOrders: 310,
+    },
+    {
+        id: 1,
+        image: "src/assets/images.jpg",
+        name: 'Tajine poulet',
+        numberOfOrders: 310,
+    }
+]
